59ac406e42ce0621a7de6c67eafdccb91d334057
Sidx005 <desaisiddharth1246@gmail.com>
2026-05-04 21:36:46 +0530

    Add cart state and handlers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [showCart,setshowCart]=useState(false)
+
+  const showCartHandler=()=>{
+    setshowCart(true)
+  }
+  const hideCartHandler=()=>{
+    setshowCart(false)
+  }
   return (
     <CartProvider>
-      <Header />
+      {showCart && <div>Cart...</div>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
